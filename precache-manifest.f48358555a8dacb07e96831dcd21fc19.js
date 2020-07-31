fc44286bd4f4469da0c8d83c768ce968eb75f6a6
DawidMmM <danonim56@gmail.com>
2020-07-31 12:09:11 +0200

    add src folder

---
+++ precache-manifest.f48358555a8dacb07e96831dcd21fc19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bd893e598b72bceb72aabcfaac523e",
+    "url": "/note/index.html"
+  },
+  {
+    "revision": "97e7a57dbe13a0b9c913",
+    "url": "/note/static/js/2.86ce52dd.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/note/static/js/2.86ce52dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8c30d65cd21a6f6962",
+    "url": "/note/static/js/main.20c7e18f.chunk.js"
+  },
+  {
+    "revision": "7bb2a8fb39e63d6a989b",
+    "url": "/note/static/js/runtime-main.49b98514.js"
+  },
+  {
+    "revision": "c720af74bc35b1f6dbabe9e58af22a57",
+    "url": "/note/static/media/iconmonstr-check-mark-2.c720af74.svg"
+  },
+  {
+    "revision": "aa72a69bfdd7308a604c3d2ac55d7d09",
+    "url": "/note/static/media/iconmonstr-pencil-9.aa72a69b.svg"
+  },
+  {
+    "revision": "6375717a86bdcd558c522aff052e27d2",
+    "url": "/note/static/media/iconmonstr-trash-can-1.6375717a.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/note/static/media/plus.e43ce6c8.svg"
+  }
+]);
